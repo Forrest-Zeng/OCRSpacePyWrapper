f7a4e835a52403120653d0b52bdc73388b42a8f3
alrond <46835649+Forrest-Zeng@users.noreply.github.com>
2021-10-06 00:20:11 +0000

    organization, created requesthandler for 429

--- OCRSpacePyWrapper/__init__.py
+++ OCRSpacePyWrapper/__init__.py
@@ -1,42 +1,11 @@
-import requests
-
-class NoDataError(Exception):
-  ...
-
-class LanguageNotFoundError(Exception):
-  ...
-
-class OCRLanguage:
-  Arabic = "ara"
-  Bulgarian = "bul"
-  Simplified_Chinese = "chs"
-  Traditional_Chinese = "cht"
-  Croatian = "hrv"
-  Czech = "cze"
-  Danish = "dan"
-  Dutch = "dut"
-  English = "eng"
-  Finnish = "fin"
-  French = "fre"
-  German = "ger"
-  Greek = "gre"
-  Hungarian = "hun"
-  Korean = "kor"
-  Italian = "ita"
-  Japanese = "jpn"
-  Polish = "pol"
-  Portuguese = "por"
-  Russian = "rus"
-  Slovenian = "slv"
-  Spanish = "spa"
-  Swedish = "swe"
-  Turkish = "tur"
-
-  languages = ["ara", "bul", "chs", "cht", "hrv", "cze", "dan", "dut", "eng", "fin", "fre", "ger", "gre", "hun", "kor", "ita", "jpn", "pol", "por", "rus", "slv", "spa", "swe", "tur"]
+from request import RequestHandler, OCR429ErrorOptions
+from errors import NoDataError, LanguageNotFoundError
+from languages import OCRLanguage
 
 class OCREngine:
   Engine1 = 1
   Engine2 = 2
+
 
 class OCRClient:
   def __init__(
@@ -51,7 +20,7 @@ class OCRClient:
     self.endpoint = endpoint
     self.key = key
 
-  def post(self, *, url=None, file=None, base64Image=None, filetype=None, OCREngine=OCREngine.Engine1, language=OCRLanguage.English, isOverlayRequired=False, detectOrientation=False, isCreateSearchablePdf=False, isSearchablePdfHideTextLayer=False, scale=False, isTable=False):
+  def post(self, *, url=None, file=None, base64Image=None, filetype=None, OCREngine=OCREngine.Engine1, language=OCRLanguage.English, isOverlayRequired=False, detectOrientation=False, isCreateSearchablePdf=False, isSearchablePdfHideTextLayer=False, scale=False, isTable=False, fail=OCR429ErrorOptions.Error):
     """
     Returns the text for a given image in JSON format in a POST request to the endpoint.
 
@@ -69,6 +38,7 @@ class OCRClient:
     :param isSearchablePdfHideTextLayer: (optional) if true, renders text layer hidden, only applies if isCreateSearchablePdf is true
     :param scale: (optional) if true, scales up low resolution files to improve quality of results
     :param isTable: (optional) if true, makes sure OCR engine parses line by line
+    :param fail: (optional) how to respond to 429 errors, 0 for raising an error, 1 for waiting until the timeout is over (often a long time), and 2 for return a string with errors
     :return: result in JSON format
     """    
     if language not in OCRLanguage.languages:
@@ -90,34 +60,50 @@ class OCRClient:
     if file:
       file_data = open(file,"rb")
       file_body["filename"] = file_data
-      return requests.post(self.endpoint, files=file_body, data=data_body).json()
+      return RequestHandler.post(self.endpoint, files=file_body, data=data_body)
     if url:
       data_body["url"] = url
-      return requests.post(self.endpoint, data=data_body).json()
+      return RequestHandler.post(self.endpoint, data=data_body)
     if base64Image:
       data_body["base64Image"] = base64Image
-      return requests.post(self.endpoint, data=data_body).json()
+      return RequestHandler.post(self.endpoint, data=data_body)
     raise NoDataError("Please supply one of file, url, or base64Image.")
 
-  def get(self, *, url=None, filetype=None, OCREngine=OCREngine.Engine1, language=OCRLanguage.English, isOverlayRequired=False, detectOrientation=False, isCreateSearchablePdf=False, isSearchablePdfHideTextLayer=False, scale=False, isTable=False):
-        if language not in OCRLanguage.languages:
-          raise LanguageNotFoundError("Invalid language.")
+  def get(self, *, url=None, filetype=None, OCREngine=OCREngine.Engine1, language=OCRLanguage.English, isOverlayRequired=False, detectOrientation=False, isCreateSearchablePdf=False, isSearchablePdfHideTextLayer=False, scale=False, isTable=False, fail=OCR429ErrorOptions.Error):
+    """
+    Returns the text for a given image in JSON format in a GET request to the imageURL endpoint.
+    
+    :param url: an absolute url path to a readable resource
+    :param filetype: (optional) overwrites the automatic file type detection
+    :param OCREngine: (optional) which OCR engine to use, see: https://ocr.space/OCRAPI#ocrengine
+    :param language: (optional) which language to use, only for engine type 1
+    :param isOverlayRequired: (optional) if true, returns the coordinates of the bounding boxes for each word
+    :param detectOrientation: (optional) if true, autorotate the image to have horizontal text
+    :param isCreateSearchablePdf: (optional) if true, generates a searchable PDF file
+    :param isSearchablePdfHideTextLayer: (optional) if true, renders text layer hidden, only applies if isCreateSearchablePdf is true
+    :param scale: (optional) if true, scales up low resolution files to improve quality of results
+    :param isTable: (optional) if true, makes sure OCR engine parses line by line
+    :param fail: (optional) how to respond to 429 errors, 0 for raising an error, 1 for waiting until the timeout is over (often a long time), and 2 for return a string with errors
+    :return: result in JSON format
+    """    
+    if language not in OCRLanguage.languages:
+      raise LanguageNotFoundError("Invalid language.")
 
-        data_body = {
-          "OCREngine": OCREngine,
-          "language": language,
-          "isOverlayRequired": isOverlayRequired,
-          "detectOrientation": detectOrientation,
-          "isCreateSearchablePdf": isCreateSearchablePdf,"isSearchablePdfHideTextLayer": isSearchablePdfHideTextLayer,
-          "scale": scale,
-          "isTable": isTable
-        }
-        if url:
-          data_body["url"] = url
-        if filetype:
-          data_body["filetype"] = filetype
-        get_url=self.endpoint+f"url?apikey={self.key}"
-        for header in data_body:
-          get_url+=f"&{header}={data_body[header]}"
-        return requests.get(get_url).json()
-        raise NoDataError("Please supply url for image")
+    data_body = {
+      "OCREngine": OCREngine,
+      "language": language,
+      "isOverlayRequired": isOverlayRequired,
+      "detectOrientation": detectOrientation,
+      "isCreateSearchablePdf": isCreateSearchablePdf,"isSearchablePdfHideTextLayer": isSearchablePdfHideTextLayer,
+      "scale": scale,
+      "isTable": isTable
+    }
+    if url:
+      data_body["url"] = url
+    if filetype:
+      data_body["filetype"] = filetype
+    url = self.endpoint + f"url?apikey={self.key}"
+    for header in data_body:
+      url += f"&{header}={data_body[header]}"
+    return RequestHandler.get(url)
+    raise NoDataError("Please supply url for image")
---
+++ OCRSpacePyWrapper/request.py
@@ -0,0 +1,41 @@
+from errors import TooManyRequestsError
+import requests, time
+
+class OCR429ErrorOptions:
+  Error = 0
+  Wait = 1
+  GentleNotification = 2
+  IgnoreError = 3
+
+class RequestHandler:
+  
+  @classmethod
+  def get(fail, *args, **kwargs):
+    response = requests.get(*args, **kwargs)
+    if response.status_code == 429:
+      if fail == 0:
+        raise TooManyRequestsError("Too many requests.")
+      if fail == 1:
+        time.sleep(int(response.headers["Retry-After"]))
+      if fail == 2:
+        return f"429 Error. Retry after {int(response.headers['Retry-After'])}"
+      if fail == 3:
+        return response.json() 
+
+    else:
+      return response.json()
+
+  @classmethod
+  def post(fail, *args, **kwargs):
+    response = requests.post(*args, **kwargs)
+    if response.status_code == 429:
+      if fail == 0:
+        raise TooManyRequestsError("Too many requests.")
+      if fail == 1:
+        time.sleep(int(response.headers["Retry-After"]))
+      if fail == 2:
+        return f"429 Error. Retry after {int(response.headers['Retry-After'])}"
+      if fail == 3:
+        return response.json() 
+    else:
+      return response.json()
